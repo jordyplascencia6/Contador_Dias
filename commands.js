07f96fc611b3881a1665854968494bf83f717189
jordy <jordyplascencia6@gmail.com>
2025-10-21 12:56:44 -0600

    Actualizacion

--- commands.js
+++ commands.js
@@ -1,6 +1,8 @@
 //creas una variable con la fecha destino 
-let fecha = new Date('December 10, 2025 00:00:00');
+let fecha = new Date('December 15, 2025 00:00:00');
 let miFecha = fecha.getTime();
+
+console.log(miFecha);
 
 let parrafoDias = document.querySelector("#dias");
 let parrafoHoras = document.querySelector("#horas");
@@ -12,6 +14,8 @@ let intervalo = setInterval(() => {
     
     //Calculas la fecha actual casa segundo
     let hoy = new Date().getTime();
+
+//    console.log(hoy);
 
     let distancia = miFecha - hoy;
 
@@ -71,21 +75,30 @@ let intervalo2 = setInterval(() => {
 
 }, 1000);
 
-let fecha3 = new Date('October 15, 2025 12:18:00');
-let miFecha3 = fecha3.getTime();
+//PARA TERMINAR TURNOOOO
 
-//let parrafoDias3 = document.querySelector("#dias3");
-let parrafoHoras3 = document.querySelector("#horas3");
-let parrafoMinutos3 = document.querySelector("#minutos3");
-let parrafoSegundos3 = document.querySelector("#segundos3");
-let cuentaAtras = document.querySelector("#cuentaAtras")
+function actualizar(){
 
-let intervalo3 = setInterval(() => {
-    
-    //Calculas la fecha actual casa segundo
-    let hoy = new Date().getTime();
+    //Se consiguen las fechas 
+    let finTurno = new Date();
+    let ahorita = new Date();
 
-    let distancia = miFecha3 - hoy;
+    finTurno.setHours(15, 0, 0, 0);
+
+    //let parrafoDias3 = document.querySelector("#dias3");
+    let parrafoHoras3 = document.querySelector("#horas3");
+    let parrafoMinutos3 = document.querySelector("#minutos3");
+    let parrafoSegundos3 = document.querySelector("#segundos3");
+    let cuentaAtras = document.querySelector("#cuentaAtras")
+
+    if(ahorita > finTurno){
+        finTurno.setDate(finTurno.getDate()+1);
+    }
+
+        //Calculas la fecha actual casa segundo
+    //    let hoy = new Date().getTime();
+
+    let distancia = finTurno - ahorita;
 
     let miDia = 1000 * 60 * 60 * 24;
     let miHoras = 1000 * 60 * 60;
@@ -97,17 +110,16 @@ let intervalo3 = setInterval(() => {
     let minutos = Math.floor((distancia % miHoras) / miMinutos);
     let segundos = Math.floor((distancia % miMinutos) / miSegundos);
 
-    //console.log(dias, horas, minutos, segundos);
+     //console.log(dias, horas, minutos, segundos);
 
     //parrafoDias3.innerText = dias;
     parrafoHoras3.innerText = horas < 10 ? "0" + horas : horas;
     parrafoMinutos3.innerText = minutos < 10 ? "0" + minutos : minutos;
     parrafoSegundos3.innerText = segundos < 10 ? "0" + segundos : segundos;
 
-    if (distancia < 0) {
-        clearInterval(intervalo3);
-        cuentaAtras.innerHTML = "<p class='grande'>¡Correeeeeeee!</p>"
+    if (distancia <= 0){
+        finTurno.setDate(finTurno.getDate()+1);
     }
+}
 
-
-}, 1000);
+setInterval(actualizar, 1000);
